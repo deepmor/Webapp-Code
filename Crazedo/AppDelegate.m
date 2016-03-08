d9cc5c9c25dd34be66aee199a7238ad06608cc08
deepmor <deep.mor91@gmail.com>
2016-03-08 10:42:58 +0530

    Initial Commit

---
+++ Crazedo/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Crazedo
+//
+//  Created by Deepak Mor on 08/03/16.
+//  Copyright © 2016 Deepak Mor. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Crazedo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Crazedo
+//
+//  Created by Deepak Mor on 08/03/16.
+//  Copyright © 2016 Deepak Mor. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
